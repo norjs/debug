ea7642053009a30668ea06d7b22c709feedca2e4
Jaakko-Heikki Heusala <jheusala@iki.fi>
2014-01-10 03:49:28 +0200

    Improved the format of log messages and implemented 300 character limit for single value output

--- src/debug.js
+++ src/debug.js
@@ -41,9 +41,28 @@ function inspect_values(x) {
 	return util.inspect(x);
 }
 
+/** Returns value with special chars converted to "\n", "\r" or "\t" */
+function convert_specials(x) {
+	return (''+x).replace(/\n/g, "\\n").replace(/\r/g, "\\r").replace(/\t/g, "\\t");
+}
+
 /** Returns value trimmed from white spaces around it */
 function trim_values(x) {
 	return (''+x).replace(/ +$/, "").replace(/^ +/, "");
+}
+
+/** Chop too long values to specified limit */
+function chop_long_values(limit) {
+	if(limit-3 < 1) {
+		throw new TypeError("limit must be at least four (4) characters!");
+	}
+	return function(x) {
+		x = ''+x;
+		if(x.length > limit) {
+			return x.substr(0, limit-3) + '...';
+		}
+		return x;
+	};
 }
 
 /** Helper function that can be called but does nothing */
@@ -75,7 +94,7 @@ Object.defineProperty(debug, 'log', {
 		return function () {
 	        var args = Array.prototype.slice.call(arguments);
 			var cols = [];
-	        util.debug( [location+':'].concat(args).map(inspect_values).map(trim_values).join(" ") );
+	        util.debug( [location+':'].concat(args).map(inspect_values).map(trim_values).map(chop_long_values(300)).map(convert_specials).join(" ") );
 		};
 	}
 });
