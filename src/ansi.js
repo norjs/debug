e82d62af3c4030408046633581e5766a2a7a570c
Jaakko-Heikki Heusala <jhh@sendanor.com>
2018-12-30 00:45:59 +0200

    Updated package configuration and initial ANSI color support

--- src/ansi.js
+++ src/ansi.js
@@ -1,3 +1,8 @@
+const PRIVATE = {
+	stream: Symbol('_stream')
+	, print: Symbol('_print')
+};
+
 /**
  * FIXME: Implement ANSI color support
  */
@@ -8,37 +13,50 @@ export default class AnsiCursor {
 	 * @param stream {object}
 	 */
 	constructor (stream) {
+		this[PRIVATE.stream] = stream;
+	}
 
+	/**
+	 *
+	 * @param codes {Array.<number>}
+	 */
+	[PRIVATE.print] (...codes) {
+		this[PRIVATE.stream].write(`\\033[${codes.join(';')}m`);
 	}
 
 	/**
 	 *
 	 */
 	brightRed () {
+		[PRIVATE.print](31, 1);
 	}
 
 	/**
 	 *
 	 */
 	brightYellow () {
+		[PRIVATE.print](33, 1);
 	}
 
 	/**
 	 *
 	 */
 	magenta () {
+		[PRIVATE.print](35);
 	}
 
 	/**
 	 *
 	 */
 	green () {
+		[PRIVATE.print](32);
 	}
 
 	/**
 	 *
 	 */
 	reset () {
+		[PRIVATE.print](0);
 	}
 
 	/**
--- src/debug.js
+++ src/debug.js
@@ -1,28 +1,30 @@
 /** Helpers for JavaScript debuging
+ *
  * Copyright (c) 2013-2019 Sendanor <info@sendanor.fi>
  * Copyright (c) 2013-2019 Jaakko-Heikki Heusala <jheusala@iki.fi>
  *
  * @FIXME: The ARRAY()'s should be converted to for-loops, etc to improve performance.
  */
 
-let DEBUG_LINE_LIMIT = parseInt(process.env.DEBUG_LINE_LIMIT || 500, 10);
+const DEBUG_LINE_LIMIT = parseInt(process.env.DEBUG_LINE_LIMIT || 500, 10);
 let NODE_ENV = process.env.NODE_ENV || 'development';
 
-let debug = module.exports = require('./core.js');
-
-let util = require("util");
-let FS = require("fs");
-let PATH = require("path");
-let is = require("nor-is");
-let ARRAY = require("nor-array");
-let FUNCTION = require("nor-function");
+import _ from 'lodash';
+import debug from './core.js';
+import util from "util";
+import FS from "fs";
+import PATH from "path";
+import is from "nor-is";
+import ARRAY from "nor-array";
 import NorAssert from './NorAssert.js';
 import DummyAssert from './DummyAssert.js';
 
-let node_0_11_or_newer = (process.versions &&
+const node_0_11_or_newer = !!(process.versions &&
 	is.string(process.versions.node) &&
-	parseFloat(process.versions.node.split('.').slice(0, 2).join('.')) >= 0.11 ) ? true : false;
-let disable_util = node_0_11_or_newer;
+	parseFloat(process.versions.node.split('.').slice(0, 2).join('.')) >= 0.11
+);
+
+const disable_util = node_0_11_or_newer;
 
 const PRIVATE = {
 	UNDEFINED: Symbol('undefined')
@@ -92,26 +94,35 @@ if (typeof Object.defineProperty === 'function') {
 	features.Object_defineProperty = true;
 }
 
-/* Pretty print paths */
+/** Pretty print paths */
 let print_path = require('./print-path.js');
 
-/* */
-debug.setProjectRoot = function(value) {
+/**
+ * @param value
+ * @return {*}
+ */
+debug.setProjectRoot = value => {
 	debug.assert(value).is('string');
 	debug.defaults.project_root = value;
 	debug.log('Paths are now relative to ', debug.defaults.project_root);
 	return debug.defaults.project_root;
 };
 
-debug.setNodeENV = function(value) {
+/**
+ *
+ * @param value
+ * @returns {string}
+ */
+debug.setNodeENV = value => {
 	NODE_ENV = (value === 'production') ? 'production' : 'development';
 	return NODE_ENV;
 };
 
 /** Set a prefix
  * @param value {String|Function} The prefix as a string or a function to build it. The function will get the default prefix as first argument.
+ * @return {String | Function | String | Function}
  */
-debug.setPrefix = function(value) {
+debug.setPrefix = value => {
 	if (!is.func(value)) {
 		debug.assert(value).is('string');
 	}
@@ -119,7 +130,10 @@ debug.setPrefix = function(value) {
 	return debug.defaults.prefix;
 };
 
-/** Get prefix */
+/** Get prefix
+ * @param value
+ * @return {*}
+ */
 function get_prefix(value) {
 	let has_prefix = debug.defaults.hasOwnProperty('prefix');
 	if (!has_prefix) {
@@ -132,7 +146,11 @@ function get_prefix(value) {
 	return ''+ value + ' ' + prefix;
 }
 
-/* Compatibility hacks */
+/** Compatibility hacks
+ * @param obj
+ * @param prop
+ * @param opts
+ */
 function _setup_property(obj, prop, opts) {
 	if (!features.Object_defineProperty) {
 		return;
@@ -142,6 +160,10 @@ function _setup_property(obj, prop, opts) {
 
 /** For optimal (v8) JIT compilation we use try-catch in own block.
  * Try-catch is also required for feature testing of imcompatible Object.defineProperty() (IE8).
+ * @param obj
+ * @param prop
+ * @param opts
+ * @param failsafe
  */
 function setup_property(obj, prop, opts, failsafe) {
 	try {
@@ -153,7 +175,7 @@ function setup_property(obj, prop, opts, failsafe) {
 
 /** Setup `debig.__stack` */
 setup_property(debug, '__stack', {
-	get: function stack_getter(){
+	get: () => {
 
 		if (!features.Error_captureStackTrace) {
 			return [];
@@ -164,7 +186,7 @@ setup_property(debug, '__stack', {
 			orig = Error.prepareStackTrace;
 			Error.prepareStackTrace = function(_, stack){ return stack; };
 			err = new Error();
-			Error.captureStackTrace(err, stack_getter);
+			Error.captureStackTrace(err, get);
 			stack = err.stack;
 		} finally {
 			Error.prepareStackTrace = orig;
@@ -174,7 +196,7 @@ setup_property(debug, '__stack', {
 }, []);
 
 setup_property(debug, '__line', {
-	get: function(){
+	get: () => {
 		let stack = debug.__stack;
 		let tmp = stack[1];
 		if (!(tmp && (typeof tmp.getLineNumber === 'function'))) {
@@ -184,38 +206,51 @@ setup_property(debug, '__line', {
 	}
 }, -1);
 
-/** Returns true if the app is running in production mode */
-debug.isProduction = function () {
-	return (NODE_ENV === "production");
-};
+/** Returns true if the app is running in production mode
+ * @return {boolean}
+ */
+debug.isProduction = () => (NODE_ENV === "production"
+);
 
-/** Returns true if the app is running in development mode */
-debug.isDevelopment = function () {
-	return debug.isProduction() ? false : true;
-};
+/** Returns true if the app is running in development mode
+ * @return {boolean}
+ */
+debug.isDevelopment = () => debug.isProduction() ? false : true;
 
-/** Returns value converted to string and trimmed from white spaces around it */
+/** Returns value converted to string and trimmed from white spaces around it
+ * @param x
+ * @return {*}
+ */
 function inspect_values(x) {
 	if (typeof x === "string") { return x; }
 	return util.inspect(x);
 }
 
-/** Returns value with special chars converted to "\n", "\r" or "\t" */
+/** Returns value with special chars converted to "\n", "\r" or "\t"
+ * @param x
+ * @return {string}
+ */
 function convert_specials(x) {
 	return (''+x).replace(/\n/g, "\\n").replace(/\r/g, "\\r").replace(/\t/g, "\\t");
 }
 
-/** Returns value trimmed from white spaces around it */
+/** Returns value trimmed from white spaces around it
+ * @param x
+ * @return {string}
+ */
 function trim_values(x) {
 	return (''+x).replace(/ +$/, "").replace(/^ +/, "");
 }
 
-/** Chop too long values to specified limit */
+/** Chop too long values to specified limit
+ * @param limit
+ * @return {Function}
+ */
 function chop_long_values(limit) {
 	if (limit-3 < 1) {
 		throw new TypeError("limit must be at least four (4) characters!");
 	}
-	return function(x) {
+	return x => {
 		x = ''+x;
 		if (x.length > limit) {
 			return x.substr(0, limit-3) + '...';
@@ -224,10 +259,13 @@ function chop_long_values(limit) {
 	};
 }
 
-/** Replace full path names */
+/** Replace full path names
+ * @param str
+ * @return {*}
+ */
 function chop_long_paths(str) {
 	str = ''+str;
-	str = str.replace(/(\/[^/:\)\(]+)+/gi, function(path) {
+	str = str.replace(/(\/[^/:\)\(]+)+/gi, path => {
 		if (FS && is.func(FS.existsSync) && FS.existsSync(path)) {
 			return print_path(path);
 		}
@@ -240,18 +278,22 @@ function chop_long_paths(str) {
 function do_nothing() {
 }
 
-/** Get timestamp */
-function get_timestamp() {
-	function dd(x) {
+/** Get timestamp
+ * @return {string}
+ */
+function get_timestamp () {
+	const dd = x => {
 		x = ''+x;
 		return (x.length === 1) ? '0'+x : x;
-	}
+	};
 	let n = new Date();
 	return n.getFullYear() + '-' + dd(n.getMonth()+1) + '-' + dd(n.getDate()) + ' ' + dd(n.getHours()) + ':' + dd(n.getMinutes()) + ':' + dd(n.getSeconds());
 }
 
-/** Write debug log message */
-function _print_error(line) {
+/** Write debug log message
+ * @param line
+ */
+function _print_error (line) {
 
 	if ( (typeof debug === 'object') && (typeof debug._log_error === 'function') ) {
 		debug._log_error( line );
@@ -285,8 +327,10 @@ function _print_error(line) {
 
 }
 
-/** Write debug log message */
-function print_error(line) {
+/** Write debug log message
+ * @param line
+ */
+function print_error (line) {
 	try {
 		if (ansi) { debug.defaults.cursors.error(stderr_cursor); }
 		_print_error(line);
@@ -295,8 +339,10 @@ function print_error(line) {
 	}
 }
 
-/** Write warning message */
-function _print_warning(line) {
+/** Write warning message
+ * @param line
+ */
+function _print_warning (line) {
 
 	if ( (typeof debug === 'object') && (typeof debug._log_warn === 'function') ) {
 		debug._log_warn( line );
@@ -330,8 +376,10 @@ function _print_warning(line) {
 
 }
 
-/** Write warning message */
-function print_warning(line) {
+/** Write warning message
+ * @param line
+ */
+function print_warning (line) {
 	try {
 		if (ansi) { debug.defaults.cursors.warning(stderr_cursor); }
 
@@ -341,8 +389,10 @@ function print_warning(line) {
 	}
 }
 
-/** Print informative log messages */
-function _print_info(line) {
+/** Print informative log messages
+ * @param line
+ */
+function _print_info (line) {
 
 	if ( (typeof debug === 'object') && (typeof debug._log_info === 'function') ) {
 		debug._log_info( line );
@@ -386,8 +436,10 @@ function _print_info(line) {
 
 }
 
-/** Print informative log messages */
-function print_info(line) {
+/** Print informative log messages
+ * @param line
+ */
+function print_info (line) {
 	try {
 		if (ansi) { debug.defaults.cursors.info(stderr_cursor); }
 
@@ -397,8 +449,10 @@ function print_info(line) {
 	}
 }
 
-/** */
-function _print_log(line) {
+/**
+ * @param line
+ */
+function _print_log (line) {
 
 	if ( (typeof debug === 'object') && (typeof debug._log === 'function') ) {
 		debug._log( line );
@@ -432,18 +486,27 @@ function _print_log(line) {
 
 }
 
-/** */
-function inspect_and_trim(v) {
+/**
+ * @param v
+ * @return {*}
+ */
+function inspect_and_trim (v) {
 	return trim_values(inspect_values(v));
 }
 
-/** */
-function chop_and_convert(v) {
+/**
+ * @param v
+ * @return {*}
+ */
+function chop_and_convert (v) {
 	return chop_long_values(DEBUG_LINE_LIMIT)(convert_specials(v));
 }
 
-/** Returns the stack property of `x` if it exists, otherwise `x` itself. */
-function get_stack(x) {
+/** Returns the stack property of `x` if it exists, otherwise `x` itself.
+ * @param x
+ * @return {*}
+ */
+function get_stack (x) {
 
 	if (!(x && x.stack)) {
 		return x;
@@ -452,11 +515,8 @@ function get_stack(x) {
 	let buf = ''+x.stack;
 	let message = buf.split('\n')[0];
 
-	let extra = Object.keys(x).filter(function(key) {
-		return (key !== 'stack') && (x[key] !== undefined);
-	}).map(function(key) {
-		return '> ' + key + ' = ' + inspect_and_trim(x[key]);
-	}).join('\n');
+	let extra = Object.keys(x).filter(key => (key !== 'stack') && (x[key] !== undefined)
+	).map(key => '> ' + key + ' = ' + inspect_and_trim(x[key])).join('\n');
 
 	if (message === ''+x) {
 		return ''+x.stack + '\n' + extra;
@@ -465,20 +525,19 @@ function get_stack(x) {
 	return '' + x + '\n' + x.stack + '\n' + extra;
 }
 
-/** */
-function failover_logger(fun) {
-	function logger() {
-		let args = Array.prototype.slice.call(arguments);
-		return fun( ARRAY(args).map(get_stack).map(inspect_and_trim).join(' ') );
-	}
-	return logger;
+/**
+ * @param fun
+ * @return {function(): *}
+ */
+function failover_logger (fun) {
+	return ( ...args ) => fun(_.map(args, item => inspect_and_trim(get_stack(item))).join(' '));
 }
 
 /** Writes debug log messages with timestamp, file locations, and function 
  * names. The usage is `debug.log('foo =', foo);`. Any non-string variable will 
  * be passed on to `util.inspect()`. */
 setup_property(debug, 'log', {
-	get: function(){
+	get: () => {
 
 		// Disable in production
 		if ( (!debug.defaults.production_enable_log) && debug.isProduction()) {
@@ -505,13 +564,11 @@ setup_property(debug, 'log', {
 			}
 		}
 
-
-		return function () {
+		return ( ...args ) => {
 			try {
 				if (ansi) { debug.defaults.cursors.log(stdout_cursor); }
-				let args = Array.prototype.slice.call(arguments);
 				_print_log( chop_long_paths(get_prefix(prefix)) + ': ');
-				ARRAY( ARRAY(args).map(inspect_and_trim).join(' ').split("\n") ).map(chop_and_convert).forEach(function(line) {
+				ARRAY( ARRAY(args).map(inspect_and_trim).join(' ').split("\n") ).map(chop_and_convert).forEach(line => {
 					_print_log( ''+ timestamp + ' > ' + chop_long_paths(line) );
 				});
 			} finally {
@@ -525,7 +582,7 @@ setup_property(debug, 'log', {
  * names. The usage is `debug.log('foo =', foo);`. Any non-string variable will
  * be passed on to `util.inspect()`. */
 setup_property(debug, 'error', {
-	get: function(){
+	get: () => {
 
 		// Disable in production
 		//if (debug.isProduction()) {
@@ -552,11 +609,10 @@ setup_property(debug, 'error', {
 			}
 		}
 
-		return function () {
-			let args = Array.prototype.slice.call(arguments);
+		return ( ...args ) => {
 			//let cols = [];
 			print_error( chop_long_paths(get_prefix(prefix)) + ': ' );
-			ARRAY( ARRAY(args).map(get_stack).map(inspect_and_trim).join(' ').split("\n") ).map(chop_and_convert).forEach(function(line) {
+			ARRAY( ARRAY(args).map(get_stack).map(inspect_and_trim).join(' ').split("\n") ).map(chop_and_convert).forEach(line => {
 				print_error( ''+timestamp + ' > ' + chop_long_paths(line) );
 			});
 		};
@@ -567,7 +623,7 @@ setup_property(debug, 'error', {
  * names. The usage is `debug.log('foo =', foo);`. Any non-string variable will
  * be passed on to `util.inspect()`. */
 setup_property(debug, 'warn', {
-	get: function(){
+	get: () => {
 
 		// Disable in production
 		//if (debug.isProduction()) {
@@ -594,11 +650,10 @@ setup_property(debug, 'warn', {
 			}
 		}
 
-		return function () {
-			let args = Array.prototype.slice.call(arguments);
+		return ( ...args ) => {
 			//let cols = [];
 			print_warning( chop_long_paths(get_prefix(prefix)) + ': ' );
-			ARRAY( ARRAY(args).map(get_stack).map(inspect_and_trim).join(' ').split("\n") ).map(chop_and_convert).forEach(function(line) {
+			ARRAY( ARRAY(args).map(get_stack).map(inspect_and_trim).join(' ').split("\n") ).map(chop_and_convert).forEach(line => {
 				print_warning( ''+timestamp + ' > ' + chop_long_paths(line) );
 			});
 		};
@@ -607,9 +662,10 @@ setup_property(debug, 'warn', {
 
 /** Writes debug log messages with timestamp, file locations, and function
  * names. The usage is `debug.log('foo =', foo);`. Any non-string variable will
- * be passed on to `util.inspect()`. */
+ * be passed on to `util.inspect()`.
+ */
 setup_property(debug, 'info', {
-	get: function(){
+	get: () => {
 
 		// Disable in production
 		//if (debug.isProduction()) {
@@ -636,26 +692,38 @@ setup_property(debug, 'info', {
 			}
 		}
 
-		return function () {
-			let args = Array.prototype.slice.call(arguments);
+		return ( ...args ) => {
 			//let cols = [];
 			print_info( chop_long_paths(get_prefix(prefix)) + ': ' );
-			ARRAY( ARRAY(args).map(get_stack).map(inspect_and_trim).join(' ').split("\n") ).map(chop_and_convert).forEach(function(line) {
+			ARRAY( ARRAY(args).map(get_stack).map(inspect_and_trim).join(' ').split("\n") ).map(chop_and_convert).forEach(line => {
 				print_info( ''+ timestamp + ' > ' + chop_long_paths(line) );
 			});
 		};
 	}
 }, failover_logger(_print_info) );
 
+/**
+ *
+ * @param value
+ * @returns {NorAssert}
+ */
 function debug_assert(value) {
 	return new NorAssert(value);
 } // debug_assert
 
-function dummy_assert() {
+/**
+ *
+ * @returns {DummyAssert}
+ */
+function dummy_assert () {
 	return new DummyAssert();
 }
 
-function assert_getter(){
+/**
+ *
+ * @returns {function(*=): NorAssert}
+ */
+function assert_getter (){
 	return debug_assert;
 } // assert_getter
 
@@ -664,27 +732,44 @@ setup_property(debug, 'assert', { get: assert_getter }, dummy_assert); // debug.
 
 /** Hijacks 3rd party method call to print debug information when it is called.
  * Use it like `debug.inspectMethod(res, 'write');` to hijack `res.write()`.
+ *
+ * @param obj {Object}
+ * @param method {string} Method name
+ * @return {Function} Reset function which will remove the inspection code.
  */
-debug.inspectMethod = function hijack_method(obj, method) {
-	let orig = obj[method];
+debug.inspectMethod = ( obj, method ) => {
+	const origSymbol = Symbol('_orig');
+
+	obj[origSymbol] = obj[method];
+
 	if (!debug.inspectMethod._id) {
 		debug.inspectMethod._id = 0;
 	}
-	obj[method] = function() {
+
+	obj[method] = (...args) => {
 		let x = (debug.inspectMethod._id += 1);
-		let args = Array.prototype.slice.call(arguments);
 		let stack = [].concat(debug.__stack);
 		debug.log('#' + x + ': Call to ' + method + ' (' + ARRAY(args).map(inspect_values).join(', ') + ') ...');
 		// FIXME: files could be printed relative to previous stack item, so it would not take that much space.
-		debug.log('#' + x + ": stack = ", ARRAY(stack).map(function(x) { return print_path(x.getFileName()) + ':' + x.getLineNumber(); }).join(' -> ') );
-		let ret = FUNCTION(orig).apply(obj, args);
+		debug.log('#' + x + ": stack = ", ARRAY(stack).map(x => print_path(x.getFileName()) + ':' + x.getLineNumber()).join(' -> ') );
+		let ret = obj[origSymbol](...args);
 		debug.log('#' + x + ': returned: ', ret);
 		return ret;
 	};
+
+	return () => {
+		obj[method] = obj[origSymbol];
+		delete obj[origSymbol];
+	};
+
 };
 
-/** */
-function get_location(x, short) {
+/**
+ * @param x
+ * @param short
+ * @return {* | * | string | string}
+ */
+function get_location (x, short) {
 	let file = (x && x.getFileName && print_path(x.getFileName())) || '';
 	let line = (x && x.getLineNumber && x.getLineNumber()) || '';
 	let func = (x && x.getFunctionName && x.getFunctionName()) || '';
@@ -699,25 +784,25 @@ function get_location(x, short) {
 	return out;
 }
 
-/** Builds a wrapper function that will print warning when an obsolete method is called
- * @params self {object} The `this` element of the method/function
- * @params obsolete_func {string} The method name that was obsolete
- * @params new_func {string} The new method name that should be used
- * @returns {function} The wrapped function that will print a warning and otherwise behave as the method would normally.
- */
-debug.obsoleteMethod = function(self, obsolete_func, func) {
-	if (typeof self !== 'function') {
-		debug.assert(self).typeOf('object');
-	}
-	debug.assert(obsolete_func).typeOf('string');
-	debug.assert(func).typeOf('string');
-	debug.assert(self[func]).typeOf('function');
-	return function() {
-		let args = Array.prototype.slice.call(arguments);
-		let stack = [].concat(debug.__stack);
-		debug.log('Warning! An obsolete method .'+ obsolete_func + '() used at ' + get_location(stack[1]) + ' -- use .' + func + '() instead.' );
-		return FUNCTION(self[func]).apply(this, args);
-	};
-};
+///** Builds a wrapper function that will print warning when an obsolete method is called
+// *
+// * @param self {object} The `this` element of the method/function
+// * @param obsolete_func {string} The method name that was obsolete
+// * @param func {string} The new method name that should be used
+// * @returns {function(): *} The wrapped function that will print a warning and otherwise behave as the method would normally.
+// */
+//debug.obsoleteMethod = ( self, obsolete_func, func ) => {
+//	if (typeof self !== 'function') {
+//		debug.assert(self).typeOf('object');
+//	}
+//	debug.assert(obsolete_func).typeOf('string');
+//	debug.assert(func).typeOf('string');
+//	debug.assert(self[func]).typeOf('function');
+//	return function (...args) {
+//		const stack = [].concat(debug.__stack);
+//		debug.log('Warning! An obsolete method .'+ obsolete_func + '() used at ' + get_location(stack[1]) + ' -- use .' + func + '() instead.' );
+//		return self[func].apply(this, args);
+//	};
+//};
 
-/* EOF */
+export default debug;
